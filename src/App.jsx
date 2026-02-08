9d109ec17438dbb62d1b9d7da529e4646260fbbd
Igor <81940547+luckdiamond0@users.noreply.github.com>
2026-02-08 20:17:38 -0300

    Change displayed name from Luckdiamond0 to Igor Sarda

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         >
           Hi, I'm{" "}
           <span className="text-primary relative">
-            Luckdiamond0
+            Igor Sarda
             <span className="absolute inset-0 bg-cyan-400/20 blur-xl -z-10"></span>
           </span>
         </motion.h1>
